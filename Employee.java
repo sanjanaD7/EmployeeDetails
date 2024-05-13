d00de70239e1a606f5c29b07b1e92162101fdbca
sanjana <01fe22bca054@kletech.ac.in>
2024-05-13 19:44:49 +0530

    pro

---
+++ Employee.java
@@ -0,0 +1,29 @@
+class Employee {
+    private String name;
+    private int employeeId;
+    private double salary;
+
+    public Employee(String name, int employeeId, double salary) {
+        this.name = name;
+        this.employeeId = employeeId;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("Employee ID: " + employeeId);
+        System.out.println("Name: " + name);
+        System.out.println("Salary: $" + salary);
+    }
+}
